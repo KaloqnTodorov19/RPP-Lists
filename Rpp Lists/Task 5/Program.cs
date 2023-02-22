22ecb3c5b409d8888e3f2a9a160cc5b3985e6b58
Kaloqn Todorov <k.todorov2019@pmg-pernik.com>
2023-02-22 09:24:28 +0200

    added tast 5 commands

---
+++ Rpp Lists/Task 5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+            while(true)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string command = input[0];
+                if(command == "print")
+                {
+                    break;
+                }
+                switch(command)
+                {
+                    case"add":
+                        {
+                            var index = int.Parse(input[1]);
+                            var element = int.Parse(input[2]);
+                            nums.Insert(index, element);
+                            break;
+                        }
+                    case "addMany":
+                        {
+
+                            break;
+                        }
+
+
+                    default:
+                    break;
+                }
+
+            }
+        }
+    }
+}
