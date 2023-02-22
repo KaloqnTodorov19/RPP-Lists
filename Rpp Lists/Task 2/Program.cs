7560dd896a94a47b6f4ff72bcccec9c9feb2fb43
Kaloqn Todorov <k.todorov2019@pmg-pernik.com>
2023-02-22 08:28:54 +0200

    Added task

--- Rpp Lists/Task 2/Program.cs
+++ Rpp Lists/Task 2/Program.cs
@@ -8,7 +8,19 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+             nums = nums.Where(x => x >= 0).ToArray();
+
+            if(nums.Length == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Array.Reverse(nums);
+                Console.WriteLine("Resulting list:", string.Join(" ", nums));
+            }
         }
     }
 }
